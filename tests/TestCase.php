376495ce85479911da28f03f5ee89fcdda2cc4cb
sanchescom <sanches.com@mail.ru>
2026-09-14 23:10:53 -0300

    test: reset paginator resolvers after testbench tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Sanchescom\Rest\Tests;
 
+use Illuminate\Pagination\Paginator;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Sanchescom\Rest\RestServiceProvider;
 
@@ -12,5 +13,14 @@ abstract class TestCase extends Orchestra
     protected function getPackageProviders($app): array
     {
         return [RestServiceProvider::class];
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // Reset paginator resolvers to prevent leak to subsequent test suites
+        Paginator::currentPathResolver(fn () => '/');
+        Paginator::currentPageResolver(fn () => 1);
     }
 }
